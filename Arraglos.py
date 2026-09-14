19d0040bbf64804bd00c0077684181a77bf3818b
Carlos-444 <juancarloscamargo770@gmail.com>
2026-09-14 17:21:09 -0600

    Se trabajo con arreglos unidimensionales cono introducción

---
+++ Arraglos.py
@@ -0,0 +1,31 @@
+
+numeros = [10, 20, 30, 40, 50]
+
+print(numeros[2])  # Imprime el tercer elemento del arreglo
+
+numeros[3] = 35  # Cambia el cuarto elemento del arreglo
+print(numeros[2])  # Imprime el arreglo modificado
+print(numeros)  # Imprime el arreglo completo
+
+numeros.append(60)  # Agrega un nuevo elemento al final del arreglo
+print(numeros)  # Imprime el arreglo completo después de agregar un elemento
+
+#obtenemos el tamaño del arreglo
+numeros.remove(35)  # Elimina el elemento con valor 35 del arreglo
+print(numeros)  # Imprime el arreglo completo después de eliminar un elemento
+
+#eliminar un elemento por su índice
+numeros.pop(4)  # Elimina el último elemento del arreglo
+print(numeros)  # Imprime el arreglo completo después de eliminar el último elemento
+
+
+
+#Arreglos de frutas
+fruta = ["manzana", "fresa", "sandia", "Mango", "Melon", "Platano"]
+fruta.pop(4)  # Elimina el cuarto elemento del arreglo
+print(fruta)  # Imprime el arreglo completo después de eliminar el cuarto elemento
+
+fruta.remove("manzana")  # Elimina el elemento "manzana" del arreglo
+print(fruta)  # Imprime el arreglo completo después de eliminar "manzana"
+
+#introduccion
