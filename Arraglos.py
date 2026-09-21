7e02574a0eec1af2ad0d546ee57e5e46065bb13b
Carlos-444 <juancarloscamargo770@gmail.com>
2026-09-21 16:50:07 -0600

    se trabajo con arreglos y se hizo un programa con Array cincuerizado

--- Arraglos.py
+++ Arraglos.py
@@ -1,4 +1,4 @@
-
+""" 
 numeros = [10, 20, 30, 40, 50]
 
 print(numeros[2])  # Imprime el tercer elemento del arreglo
@@ -28,4 +28,52 @@ print(fruta)  # Imprime el arreglo completo después de eliminar el cuarto eleme
 fruta.remove("manzana")  # Elimina el elemento "manzana" del arreglo
 print(fruta)  # Imprime el arreglo completo después de eliminar "manzana"
 
-#introduccion
+#declaracion de arreglo
+arreglo = []
+print(fruta)
+
+#declaracion de arreglo con tamaño definido
+n = int(input("Ingrese la cantidad de números que desea agregar al arreglo: "))
+print(n)
+
+for i in range(n):
+    dato = int(input("Ingrese un número: "))
+    arreglo.append(dato)  # Agrega el número ingresado al arreglo
+    print(arreglo)  # Imprime el arreglo completo después de agregar cada número
+
+#declaracion de un arreglo de 15 elementos
+n = int(input("Ingrese la cantidad de números que desea agregar al arreglo "))
+print(n)
+
+for i in range(n):
+    dato = int(input("Ingrese un número: "))
+    arreglo.append(dato)  # Agrega el número ingresado al arreglo
+    print(arreglo)  # Imprime el arreglo completo después de agregar cada número
+    
+"""
+
+    
+# Declaramos un arreglo vacío
+
+arreglo = []
+
+# Pedimos 15 números
+
+for i in range(15):
+    dato = int(input("Ingrese un número entre 0 y 500: "))
+    arreglo.append(dato)
+
+print("Array original:")
+print(arreglo)
+
+
+for i in range(15):
+
+    # Verificamos si el número no es múltiplo de 5
+    if arreglo[i] % 5 != 0:
+
+        # Si no es múltiplo de 5, lo ajustamos al siguiente múltiplo de 5
+        arreglo[i] = arreglo[i] + (5 - arreglo[i] % 5)
+
+print("Array cincuerizado:")
+print(arreglo)
